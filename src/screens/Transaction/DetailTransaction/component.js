daf6fea4d9855f326e9efa85bdfd7c2c1044141c
Adityawan Chandra <adityawancha@gmail.com>
2021-05-09 12:19:42 +0700

    Update Transaction Page

--- src/screens/Transaction/DetailTransaction/component.js
+++ src/screens/Transaction/DetailTransaction/component.js
@@ -1,5 +1,8 @@
+/* eslint-disable camelcase */
 import React from 'react';
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity, Image, TextInput, ScrollView } from 'react-native';
+
+import { Navigation } from 'react-native-navigation';
 
 import PropTypes from 'prop-types';
 
@@ -10,8 +13,83 @@ import Icon from '../../../assets/png/img-bags.png';
 import styles from './styles';
 
 class ListProduct extends React.Component {
+  state = {
+    status: ['New Order', 'Pending', 'Approved', 'Delivered', 'Canceled'],
+    active: undefined,
+    no_resi: null
+  };
+
+  _handleBack = () => {
+    const { componentId } = this.props;
+
+    Navigation.pop(componentId);
+  };
+
+  _handleSubmit = () => {
+    const { actions, selectedData, lastStatus } = this.props;
+    const { active, no_resi } = this.state;
+
+    const selectedStatus = active === undefined ? lastStatus : active;
+
+    actions.updateTransactions(
+      selectedData.id,
+      { status: selectedStatus.toString(), no_resi },
+      actions.getTransactions(lastStatus, this._handleBack)
+    );
+  };
+
+  _handleChangeActive = newActive => () => {
+    this.setState({ active: newActive });
+  };
+
+  _handleChangeNoResi = value => {
+    this.setState({ no_resi: value });
+  };
+
+  _renderRadio = active => (item, idx) => {
+    const { lastStatus } = this.props;
+    const selectedStatus = active === undefined ? lastStatus : active;
+
+    return (
+      <TouchableOpacity
+        disabled={active === idx}
+        onPress={this._handleChangeActive(idx)}
+        key={idx}
+        style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}
+      >
+        <View
+          style={{
+            width: 20,
+            aspectRatio: 1,
+            borderWidth: 2,
+            borderColor: 'rgba(0,0,0,0.1)',
+            borderRadius: 10,
+            marginRight: 10
+          }}
+        >
+          {selectedStatus === idx && (
+            <View
+              style={{
+                flex: 1,
+                aspectRatio: 1,
+                backgroundColor: '#DB3022',
+                borderRadius: 10,
+                borderWidth: 1,
+                borderColor: '#fff'
+              }}
+            />
+          )}
+        </View>
+        <Text style={{ fontSize: 15 }}>{item}</Text>
+      </TouchableOpacity>
+    );
+  };
+
   render() {
-    const { profile } = this.props;
+    const { profile, selectedData, lastStatus } = this.props;
+    const { status, active } = this.state;
+
+    const selectedStatus = active === undefined ? lastStatus : active;
 
     const iconSize = Image.resolveAssetSource(Icon);
 
@@ -30,35 +108,67 @@ class ListProduct extends React.Component {
         <View style={styles.content}>
           <Text style={styles.title}>Transaction Detail</Text>
 
-          <View
-            style={{
-              backgroundColor: '#fff',
-              marginHorizontal: 8,
-              paddingVertical: 53,
-              alignItems: 'center',
-              marginTop: 59,
-              elevation: 4,
-              borderRadius: 5
-            }}
-          >
-            <View style={{ width: 190, aspectRatio: iconSize.width / iconSize.height, marginBottom: 20 }}>
-              <Image source={Icon} style={{ width: null, height: null, flex: 1, resizeMode: 'contain' }} />
-            </View>
-
-            <Text style={{ fontSize: 18, textAlign: 'center' }}>{'Order needs confirm from\nyou!'}</Text>
-
-            <TouchableOpacity
+          <ScrollView contentContainerStyle={{ paddingHorizontal: 34 }}>
+            <View
               style={{
-                backgroundColor: '#DB3022',
-                width: '65%',
-                paddingVertical: 8,
-                borderRadius: 3,
-                marginTop: 20
+                backgroundColor: '#fff',
+                marginTop: 20,
+                marginBottom: 20,
+                elevation: 2,
+                borderRadius: 5,
+                paddingVertical: 53
               }}
             >
-              <Text style={{ textAlign: 'center' }}>Save</Text>
-            </TouchableOpacity>
-          </View>
+              <View
+                style={{
+                  alignSelf: 'center',
+                  width: 190,
+                  aspectRatio: iconSize.width / iconSize.height,
+                  marginBottom: 20
+                }}
+              >
+                <Image source={Icon} style={{ width: null, height: null, flex: 1, resizeMode: 'contain' }} />
+              </View>
+
+              <Text style={{ fontSize: 18, textAlign: 'center' }}>{'Order needs confirm from\nyou!'}</Text>
+
+              <View style={{ marginHorizontal: 50, marginTop: 20 }}>
+                {status.map(this._renderRadio(active))}
+              </View>
+
+              {selectedStatus === 2 && (
+                <View
+                  style={{
+                    marginHorizontal: 45,
+                    borderWidth: 1.5,
+                    borderColor: 'rgba(0,0,0,0.1)',
+                    borderRadius: 3,
+                    marginTop: 5
+                  }}
+                >
+                  <TextInput
+                    defaultValue={selectedData?.no_resi}
+                    placeholder="No. Resi"
+                    onChangeText={this._handleChangeNoResi}
+                    style={{ padding: 0, paddingHorizontal: 8 }}
+                  />
+                </View>
+              )}
+
+              <TouchableOpacity
+                onPress={this._handleSubmit}
+                style={{
+                  backgroundColor: '#DB3022',
+                  paddingVertical: 8,
+                  borderRadius: 3,
+                  marginTop: 20,
+                  marginHorizontal: 45
+                }}
+              >
+                <Text style={{ textAlign: 'center', color: '#fff' }}>Save</Text>
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
         </View>
       </View>
     );
@@ -66,11 +176,19 @@ class ListProduct extends React.Component {
 }
 
 ListProduct.defaultProps = {
-  profile: {}
+  componentId: 'detailtransactionscreen',
+  profile: {},
+  lastStatus: 0,
+  selectedData: {},
+  actions: {}
 };
 
 ListProduct.propTypes = {
-  profile: PropTypes.object
+  componentId: PropTypes.string,
+  profile: PropTypes.object,
+  lastStatus: PropTypes.number,
+  selectedData: PropTypes.object,
+  actions: PropTypes.object
 };
 
 export default ListProduct;
